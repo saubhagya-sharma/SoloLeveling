e29d8ccce81eec850bdb28ee6c0986201dd436a8
Saubhagya Sharma <81666054+saubhagya-sharma@users.noreply.github.com>
2026-03-02 22:40:32 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/sololeveling/MainActivity.kt
+++ app/src/main/java/com/example/sololeveling/MainActivity.kt
@@ -5,10 +5,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         startActivity(Intent(this, AcceptPlayerActivity::class.java))
         finish()
     }
